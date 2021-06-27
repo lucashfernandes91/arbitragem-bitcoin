b2206b001388d939e42fde6cdd590617f31c6d20
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2021-06-27 18:32:45 -0300

    Atualização das Exchanges

--- app.py
+++ app.py
@@ -3,38 +3,41 @@ import requests, json
 import time, datetime
 import numpy
 
-spread_desejado = 1 # PREENCHER CAMPO APENAS COM O VALOR INTERNO, DESCONSIDERANDO A (%)
+spread_desejado = 0.1 # PREENCHER CAMPO APENAS COM O VALOR INTERNO, DESCONSIDERANDO A (%)
 
 while True:
     requisicao = requests.get('https://api.bitvalor.com/v1/ticker.json')
     informacoes = json.loads(requisicao.text)
 
+
     # RETORNA O VALOR DO BITCOIN EM CADA EXCHANGE
     exchange = {}
-    exchange['Walltime'] = informacoes['ticker_24h']['exchanges']['WAL']['last']
     exchange['Mercado_Bitcoin'] = informacoes['ticker_24h']['exchanges']['MBT']['last']
-    exchange['BitCambio'] = informacoes['ticker_24h']['exchanges']['CAM']['last']
-    exchange['BZX'] = informacoes['ticker_24h']['exchanges']['BZX']['last']
     exchange['BitcoinTrade'] = informacoes['ticker_24h']['exchanges']['BTD']['last']
+    exchange['Bisq'] = informacoes['ticker_24h']['exchanges']['BSQ']['last']
+
 
     # ATRIBUI O PREÇO DO BITCOIN ÁS VARIÁVEIS
-    walltime = exchange['Walltime']
     mbitcoin = exchange['Mercado_Bitcoin']
-    bitcambio = exchange['BitCambio']
-    bzx = exchange['BZX']
     bitcointrade = exchange['BitcoinTrade']
-    
+    bisq = exchange['Bisq']
+
+
     # CRIA UMA TUPLA COM OS TODOS OS PREÇOS DE BITCOIN
-    valores = (walltime, mbitcoin, bitcambio, bzx, bitcointrade)
+    valores = (mbitcoin, bitcointrade)
+
 
     # RETORNA O MENOR PREÇO
     menor_preco = numpy.amin(valores)
 
+
     # RETORNA O MAIOR PREÇO
     maior_preco = numpy.amax(valores)
 
+
     # CALCULA A DIFERENCA ENTRE OS PRECOS (EM PORCENTAGEM)
     spread = (((maior_preco - menor_preco) * 100) / menor_preco)
+
 
     # VERIFICA SE O SPREAD DO MERCADO É MAIOR QUE O SOLICITADO
     if spread >= spread_desejado:
@@ -45,12 +48,12 @@ while True:
         # VERIFICA O NOME DE QUAL EXCHANGE O PREÇO ESTÁ MENOR
         for key in exchange:
             if exchange[key] == menor_preco:
-                print("MENOR preço em {}: {}".format(key, exchange[key]))
+                print("MENOR preço em >>{}: {}<<".format(key, exchange[key]))
 
         # VERIFICA O NOME DE QUAL EXCHANGE O PREÇO ESTÁ MAIOR
         for key in exchange:
             if exchange[key] == maior_preco:
-                print("MAIOR preço em {}: {}".format(key,exchange[key]))
+                print("MAIOR preço em >>{}: {}<<".format(key,exchange[key]))
         break
     time.sleep(60)
 
